c4bc05b1ac2fabd1ee745fe1f5375f62966c3014
semirazov <semirazov94@gmail.com>
2017-04-06 12:39:36 +0300

    finished responsive markup

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,47 @@
     $(function() {
         $('#closePromoBar').on('click', function() {
             $('.page-header__promo').slideToggle(300, 'swing');
+        });
+
+        $('.spinner .btn:first-of-type').on('click', function() {
+            var btn = $(this);
+            var input = btn.closest('.spinner').find('input');
+            if (input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {
+                input.val(parseInt(input.val(), 10) + 1);
+            } else {
+                btn.next("disabled", true);
+            }
+        });
+        $('.spinner .btn:last-of-type').on('click', function() {
+            var btn = $(this);
+            var input = btn.closest('.spinner').find('input');
+            if (input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {
+                input.val(parseInt(input.val(), 10) - 1);
+            } else {
+                btn.prev("disabled", true);
+            }
+        });
+
+        $('.spinner input').on('change', function() {
+            var val = $(this).val();
+            if (val == '' || !val.match(/^\d$/gm)) {
+                $(this).val($(this).attr('min'));
+            }
+        })
+
+        $("#clearSelection").on('click', function(e) {
+            e.preventDefault();
+            $("select").selectpicker('deselectAll');
+            var spinner = $('.spinner input');
+            spinner.each(function() {
+                $(this).val($(this).attr('min'));
+            });
+        });
+
+        $('.gallery__thumbs').on('click', function(e) {
+            var $target = $(e.target);
+            $('.gallery__thumbs-item').removeClass('active');
+            $target.parent().addClass('active');
         })
     });
 })();
